b7690a47bf948413fc0d51c37ccab0d1a3c7f0c2
Your Name <webdiegomassarini@gmail.com>
2021-03-23 00:31:48 +1100

    adding infinite scroll

--- src/components/Fetch.tsx
+++ src/components/Fetch.tsx
@@ -14,36 +14,49 @@ interface INews {
 }
 
 const Fetch = ({ country, category }: SearchProps) => {
-  const [news, setNews] = useState<Object[]>();
-  const [pageNumber, setPageNumber] = useState<Number>(1)
-  const [loading,setLoading]= useState<boolean>(true)
-  const [error,setError]=useState<boolean>(false)
+  const [news, setNews] = useState<object[]>();
+  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
 
   useEffect(() => {
-    setLoading(true)
-    setError(false)
+    setLoading(true);
+    setError(false);
     axios({
       method: "GET",
       url: `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${process.env.REACT_APP_NEWS_API}&page=${pageNumber}`,
-    }).then((res) => {
-      console.log(res)
-      setNews(res.data.articles);
-      setLoading(false)
-    }).catch(err=>{
-      setError(true)
-    });
-  }, [country, category,pageNumber]);
-  
+    })
+      .then((res) => {
+        // console.log(res);
+        setNews(res.data.articles);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(true);
+      });
+  }, [country, category, pageNumber]);
+
+  const handleScroll = (e: any) => {
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+    console.log(scrollTop, "scroll top");
+    console.log(clientHeight, "clientHeight");
+    console.log(scrollHeight, "scrollHeight");
+    if (scrollHeight - (clientHeight + scrollTop) === 0) {
+      setPageNumber(2);
+      // setLoading(true);
+
+    }
+  };
+
   return (
-      
-    <div>
-      {loading && 'Loading...👴'}
+    <div onScroll={handleScroll} style={{ overflow: "auto", height: "100vh" }}>
+      {loading && "Loading...👴"}
       {error && "Error...👷‍♂️"}
 
-      {news && !loading &&  (
+      {news && !loading && (
         <div>
           {news.map((a: INews, index: number) => {
-            console.log(a);
+            // console.log(a);
             return <News key={index} Title={a.title} Content={a.content} Img={a.urlToImage} />;
           })}
         </div>
--- src/style/Global.style.tsx
+++ src/style/Global.style.tsx
@@ -8,15 +8,15 @@ const GlobalStyle = createGlobalStyle`
   box-sizing:border-box;
 }
 body{
-  font-family: "Consolas-Normal", sans-serif; 
+  /* font-family: "Consolas-Normal", sans-serif;  */
   font-size:62,5%;
 }
 
 
-@font-face {
+/* @font-face {
   font-family: "Consolas-Normal";
   src: url("../Fonts/consola.woff") format("woff"),
       url("../Fonts/consola.ttf") format("truetype");
-}
+} */
 `
 export default GlobalStyle;
